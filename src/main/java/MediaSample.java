1156e33f43e78b0c63c62f4d900aa2943730fe57
CJ Miller <cjmiller721@gmail.com>
2017-04-17 16:49:02 -0400

    Inital Commit

---
+++ src/main/java/MediaSample.java
@@ -0,0 +1,40 @@
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import javafx.stage.Stage;
+import uk.co.caprica.vlcj.binding.LibVlc;
+import uk.co.caprica.vlcj.component.EmbeddedMediaPlayerComponent;
+import uk.co.caprica.vlcj.discovery.NativeDiscovery;
+import uk.co.caprica.vlcj.player.media.callback.seekable.RandomAccessFileMedia;
+
+import javax.swing.*;
+import java.io.File;
+
+/**
+ * Created by CJ on 3/31/2017.
+ */
+public class MediaSample extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        boolean found = new NativeDiscovery().discover();
+        System.out.println(found);
+        System.out.println(LibVlc.INSTANCE.libvlc_get_version());
+
+        EmbeddedMediaPlayerComponent mediaPlayerComponent = new EmbeddedMediaPlayerComponent();
+
+        JFrame frame = new JFrame("My First Media Player");
+        frame.setBounds(100, 100, 600, 400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setContentPane(mediaPlayerComponent);
+        frame.setVisible(true);
+
+        mediaPlayerComponent.getMediaPlayer().playMedia(new RandomAccessFileMedia(new File("F:\\Recordings\\WoW\\Raid\\2016-10-13 20-33-59.flv"), 1024*1024));
+    }
+}
---
+++ src/main/java/Tutorial.java
@@ -0,0 +1,34 @@
+import uk.co.caprica.vlcj.component.EmbeddedMediaPlayerComponent;
+import uk.co.caprica.vlcj.discovery.NativeDiscovery;
+import uk.co.caprica.vlcj.player.media.callback.seekable.RandomAccessFileMedia;
+
+import javax.swing.*;
+import java.io.File;
+
+public class Tutorial {
+
+    private final JFrame frame;
+
+    private final EmbeddedMediaPlayerComponent mediaPlayerComponent;
+
+    public static void main(String[] args) {
+        new NativeDiscovery().discover();
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new Tutorial();
+            }
+        });
+    }
+
+    public Tutorial() {
+        frame = new JFrame("My First Media Player");
+        frame.setBounds(100, 100, 600, 400);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mediaPlayerComponent = new EmbeddedMediaPlayerComponent();
+        frame.setContentPane(mediaPlayerComponent);
+        frame.setVisible(true);
+
+        mediaPlayerComponent.getMediaPlayer().playMedia(new RandomAccessFileMedia(new File("F:\\Recordings\\WoW\\Raid\\2016-10-13 20-33-59.flv"), 1024*1024));
+        //mediaPlayerComponent.getMediaPlayer().playMedia("http://portalnumber9.com/TEMP/testFile.flv");
+    }
+}
