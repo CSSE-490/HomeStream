7aca99208d817d12ddc4a4925a59c3dcf2d1c54d
Jesse Shellabarger <shellajt@rose-hulman.edu>
2017-04-26 18:08:48 -0400

    implemented repl

--- src/main/java/main/Startup.java
+++ src/main/java/main/Startup.java
@@ -3,9 +3,10 @@ package main;
 import networking.Host;
 import networking.MessageHandler;
 import networking.Server;
-import networking.protocol.IdentificationSend;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * Created by Jesse Shellabarger on 4/26/2017.
@@ -21,6 +22,34 @@ public class Startup {
             Host host = new Host(networkHost);
             MessageHandler handler = new MessageHandler(host);
             handler.identify();
+        }
+
+        repl();
+    }
+
+    private static void repl() {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            String line = reader.readLine();
+
+            switch (line) {
+                case "help":
+                    System.out.println("COMMANDS: ");
+                    break;
+                case "connect":
+                    System.out.println("Please enter the hostname:port of an existing node of the network");
+                    String hostname = reader.readLine();
+                    String[] args = hostname.split(":");
+                    MessageHandler handler = new MessageHandler(new Host(args[0], Integer.parseInt(args[1])));
+                    handler.identify();
+                    break;
+            }
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
